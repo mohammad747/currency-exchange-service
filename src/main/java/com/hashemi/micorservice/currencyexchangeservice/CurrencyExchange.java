2409034004730d8ff9e3db31a3854c1b79e3544e
mohammad hashemi <mohammad.hashemi747@gmail.com>
2023-07-28 13:41:55 +0330

    create currency-exchange-service

---
+++ src/main/java/com/hashemi/micorservice/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,27 @@
+package com.hashemi.micorservice.currencyexchangeservice;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CurrencyExchange {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "currency_from")
+    private String from;
+    @Column(name = "currency_to")
+    private String to;
+    private BigDecimal conversionMultiple;
+    private String environment;
+}
